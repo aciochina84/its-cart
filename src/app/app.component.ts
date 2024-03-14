bbc82a74caedfa466493235810fa02804689f783
Andrei Ciochina <a.ciochina@gmail.com>
2024-03-14 12:56:54 +0100

    cart-item angular part1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CART } from './cart';
+import { getDiscountAmount, getDiscountedPrice, getPrice, getVAT, parseItem } from './cart-utils';
+import { CartItem } from './cart-item.entity';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'its-cart';
+  items = CART;
+  vat = getVAT('IT');
+  getItemPrice(item: CartItem) {
+    const discountedPrice = getDiscountedPrice(item.netPrice, item.discount);
+    return getPrice(discountedPrice * item.quantity, this.vat);
+  }
+
+  getDiscountAmount(item: CartItem) {
+    return getDiscountAmount(item.netPrice, item.discount) * item.quantity;
+  }
 }
